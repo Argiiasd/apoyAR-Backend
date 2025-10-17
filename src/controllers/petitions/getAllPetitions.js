45e9bfcdf565a152d8a0f8517d0cd16572f693d6
Mateo P. <mateo.p.giacosa@gmail.com>
2025-10-17 17:43:18 -0300

    Feat(petitions): Añadido controlador, ruta y handler para recibir todas las peticiones

---
+++ src/controllers/petitions/getAllPetitions.js
@@ -0,0 +1,26 @@
+const { petition } = require("../../db");
+
+const getAllPetitions = async () => {
+  try {
+    const foundPetitions = await petition.findAll();
+
+    if (!foundPetitions || foundPetitions.length == 0) {
+      return {
+        success: false,
+        message: "No se encontraron peticiones",
+      };
+    } else {
+      return {
+        success: true,
+        petitions: foundPetitions,
+      };
+    }
+  } catch (error) {
+    return {
+      success: false,
+      message: "Error al recopilar peticiones",
+    };
+  }
+};
+
+module.exports = getAllPetitions;
